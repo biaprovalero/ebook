4258ffe94babfd18a05ed163d25d57cf26fac381
Beatriz Rodrigues Batista Provalero <beatrizrprovalero@hotmail.com>
2025-01-17 08:16:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.getElementById('buybutton').addEventListener('click', function() {
-    // Substitua a URL abaixo pela URL real da sua página de compra
+document.getElementById('buyButton').addEventListener('click', function () {
+    // Substitua pela URL da página de compra
     window.location.href = 'https://pay.kirvano.com/4ffd7ac7-9d9d-4db3-8a41-f664fc794c73';
 });
